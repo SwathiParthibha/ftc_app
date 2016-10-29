190cb6abcb5b513a559dea26f167be98b1acdd61
Pranav Sivaraman <pranavsivaraman@gmail.com>
2016-10-29 19:07:39 -0400

    Added Mecanum Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pranav/Mecanum_Go_Straight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pranav/Mecanum_Go_Straight.java
@@ -27,6 +27,7 @@ package org.firstinspires.ftc.teamcode.Pranav;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous(name = "Mecanum Autonomous", group = "Autonomous")
 //@Disabled
@@ -34,27 +35,97 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 /*
 This is an example of a Basic Autonomous utilizing the functions from RobotHardware Class
  */
+
 public class Mecanum_Go_Straight extends LinearOpMode
 {
+    DcMotor frontRight;
+    DcMotor backRight;
+    DcMotor frontLeft ;
+    DcMotor backLeft;
+
+    int ROTATION = 1220;
+    int SECOND = 1000;
+
+    int MOTOR_POWER = 1;
+
+    public void stopRobot()
+    {
+        frontRight.setPower(MOTOR_POWER * 0);
+        backRight.setPower(MOTOR_POWER * 0);
+        frontLeft.setPower(MOTOR_POWER * 0);
+        backLeft.setPower(MOTOR_POWER * 0);
+    }
+
+    public void drive(int Distance, double Speed) throws InterruptedException
+    {
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontRight.setPower(MOTOR_POWER * Speed);
+        backRight.setPower(MOTOR_POWER * Speed);
+        frontLeft.setPower(MOTOR_POWER * Speed);
+        backLeft.setPower(MOTOR_POWER * Speed);
+
+        frontRight.setTargetPosition(Distance);
+        backRight.setTargetPosition(Distance);
+        frontLeft.setTargetPosition(Distance);
+        backLeft.setTargetPosition(Distance);
+
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while (frontRight.isBusy() && backRight.isBusy() && frontLeft.isBusy() && backLeft.isBusy())
+        {
+
+        }
+
+        stopRobot();
+
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
   @Override
   public void runOpMode() throws InterruptedException
   {
-    //Use this command to access the all the functions in the RobotHardware Class
-    MecanumHardware robot= new MecanumHardware();
-    //Use this command to initialize the robot in the RobotHardware Class
-    robot.init(hardwareMap);
+      frontRight = hardwareMap.dcMotor.get("motor_2");
+      backRight = hardwareMap.dcMotor.get("motor_4");
+      frontLeft = hardwareMap.dcMotor.get("motor_3");
+      backLeft = hardwareMap.dcMotor.get("motor_1");
 
-      // wait for the start button to be pressed.
+      frontRight.setDirection(DcMotor.Direction.FORWARD);
+      backRight.setDirection(DcMotor.Direction.FORWARD);
+      frontLeft.setDirection(DcMotor.Direction.REVERSE);
+      backLeft.setDirection(DcMotor.Direction.REVERSE);
+
+      // Set all motors to zero power
+      frontRight.setPower(0);
+      backRight.setPower(0);
+      frontLeft.setPower(0);
+      backLeft.setPower(0);
+
+      // Set all motors to run without encoders.
+      // May want to use RUN_USING_ENCODERS if encoders are installed.
+      frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+      backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+      frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+      backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
       waitForStart();
       {
-          //An example of using the Drive function from the RobotHardware Class
-          robot.drive(robot.ROTATION * 2, 0.5);
-
-          //A little bit of settling time
-          sleep(500);
-
-          //An example of using the Turn function from the RobotHardware Class
-          //robot.Turn("right", 45, 0.5);
+          drive(ROTATION * 2, 1.0);
       }
   }
 }
